b6665bf4faeff1a9591e4290e6178fdc12554dc2
FaithThirteen <68504564+FaithThirteen@users.noreply.github.com>
2022-03-01 15:37:13 +0800

    feat:2022-3-1,2道

---
+++ tree/236-gg.go
@@ -0,0 +1,37 @@
+package main
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+// 此题gg，https://github.com/youngyangyang04/leetcode-master/blob/master/problems/0236.%E4%BA%8C%E5%8F%89%E6%A0%91%E7%9A%84%E6%9C%80%E8%BF%91%E5%85%AC%E5%85%B1%E7%A5%96%E5%85%88.md
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	if root == p || root == q {
+		return root
+	}
+
+	left := lowestCommonAncestor(root.Left, p, q)
+	right := lowestCommonAncestor(root.Right, p, q)
+
+	if left != nil && right != nil {
+		return root
+	}
+
+	if left != nil {
+		return left
+	}
+
+	if right != nil {
+		return right
+	}
+
+	return nil
+}
--- tree/501.go
+++ tree/501.go
@@ -1,2 +1,75 @@
 package main
 
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+// 暴力解法
+func findMode(root *TreeNode) []int {
+	history := make(map[int]int)
+	var f func(r *TreeNode)
+	f = func(r *TreeNode) {
+		if r == nil {
+			return
+		}
+		f(r.Left)
+		if _, ok := history[r.Val]; !ok {
+			history[r.Val] = 1
+		} else {
+			history[r.Val]++
+		}
+		f(r.Right)
+	}
+	f(root)
+
+	var maxValue int
+	var res []int
+	for _, v := range history {
+		if v > maxValue{
+			maxValue = v
+		}
+	}
+
+	for k, v := range history {
+		if maxValue == v{
+			res = append(res,k)
+		}
+	}
+
+	return res
+}
+
+func findMode1(root *TreeNode) []int {
+
+	var pre *TreeNode
+	var f func(r *TreeNode)
+	var max = 1
+	var count int
+	var res []int
+	f = func(cur *TreeNode) {
+		if cur == nil {
+			return
+		}
+		f(cur.Left)
+		if pre != nil && pre.Val == cur.Val{
+			count ++
+		}else{
+			count = 1
+		}
+		if count > max {
+			res = []int{cur.Val}
+			max = count
+		}else if count == max{
+			res = append(res,cur.Val)
+		}
+		pre = cur
+		f(cur.Right)
+	}
+	f(root)
+	return res
+}
