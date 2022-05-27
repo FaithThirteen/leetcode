687dd48a6b65068e9f0afff8b41b864f2ac301f6
FaithThirteen <68504564+FaithThirteen@users.noreply.github.com>
2022-05-27 16:44:41 +0800

    feat:注释

--- fuxi/jianzhioffer/Jz36.go
+++ fuxi/jianzhioffer/Jz36.go
@@ -8,11 +8,8 @@ package main
  * }
  */
 
-/**
- *
- * @param pRootOfTree TreeNode类
- * @return TreeNode类
- */
+// Convert
+// 因为要成为有序的双向链表，因为是二叉搜索树，可以采用中序遍历，就是天然的从大到小
 func Convert( pRootOfTree *TreeNode ) *TreeNode {
 	// write code here
 	var head,pre *TreeNode
@@ -24,14 +21,15 @@ func Convert( pRootOfTree *TreeNode ) *TreeNode {
 		}
 
 		dfs(cur.Left)
+
 		if pre == nil { // 此时pre为null,这是最左节点,即新的节点
 			head = cur
-		}else{ //
+		}else{ // 如果不为nil说明已经不是最左节点
 			pre.Right = cur
 			cur.Left = pre
 		}
 
-		pre = cur
+		pre = cur // 更新上驱节点
 
 		dfs(cur.Right)
 	}
